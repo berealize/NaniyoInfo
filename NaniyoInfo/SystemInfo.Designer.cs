fab88fe815522aa45c810b2935a4c56e8366bfd7
20230206 <berealize@gmail.com>
2024-12-30 16:32:06 +0900

    초기 커밋.

---
+++ NaniyoInfo/SystemInfo.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace NaniyoInfo
+{
+    partial class SystemInfo
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtInfo.Font = new System.Drawing.Font("맑은 고딕", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtInfo.Location = new System.Drawing.Point(0, 0);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInfo.Size = new System.Drawing.Size(1039, 822);
+            this.txtInfo.TabIndex = 0;
+            // 
+            // SystemInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 822);
+            this.Controls.Add(this.txtInfo);
+            this.Name = "SystemInfo";
+            this.Text = "Naniyo System Information";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInfo;
+    }
+}
+
---
+++ NaniyoInfo/SystemInfo.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Management;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NaniyoInfo
+{
+    public partial class SystemInfo : Form
+    {
+        public SystemInfo()
+        {
+            InitializeComponent();
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(GetComputerName());
+            sb.AppendLine(GetOSName());
+            sb.AppendLine(GetMotherBoardInfo());
+            sb.AppendLine(GetCPUInfo());
+            sb.AppendLine(GetGPUInfo());
+            sb.AppendLine(GetDiskInfo());
+            sb.AppendLine(GetRAMInfo());
+            sb.AppendLine(GetNetworkAdapter());
+
+            txtInfo.Text = sb.ToString();
+            txtInfo.SelectionStart = txtInfo.Text.Length;
+        }
+
+        public string GetComputerName()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(string.Format("Computer Name : {0}\r\nDomain Group : {1}\r\nLogon Server : {2}\r\nLogon User : {3}", 
+                            System.Environment.MachineName, 
+                            System.Environment.UserDomainName, 
+                            System.Environment.GetEnvironmentVariable("LogonServer").Replace("\\", ""), 
+                            System.Environment.UserName));
+
+            return sb.ToString();
+        }
+
+        public string GetOSName()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(string.Format("OS Version : {0} ({1})\r\nOS Location : {2}",
+                                System.Environment.OSVersion.VersionString,
+                                (System.Environment.Is64BitOperatingSystem ? "x64" : "x86"),
+                                System.Environment.SystemDirectory.Replace("\\system32", "")));
+
+            return sb.ToString();
+        }
+
+        public string GetRAMInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT TotalPhysicalMemory FROM Win32_ComputerSystem");
+
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                ulong u1Bytes = Convert.ToUInt64(obj["TotalPhysicalMemory"]);
+                ulong u1MB = u1Bytes / (1024 * 1024);
+                ulong u1GB = u1MB / 1024;
+
+                sb.AppendLine(string.Format("Memory : {0} MB ({1} GB)", u1MB.ToString(), u1GB.ToString()));
+            }
+
+            return sb.ToString();
+        }
+
+
+        public string GetMotherBoardInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_BaseBoard");
+
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                sb.AppendLine(string.Format("Mother Board : {0} {1} {2}", obj["Manufacturer"], obj["Product"], obj["SerialNumber"]));
+            }
+
+            searcher = new ManagementObjectSearcher("SELECT * FROM Win32_BIOS ");
+
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                sb.AppendLine(string.Format("Bios Info : {0} {1}", obj["Description"], obj["SerialNumber"]));
+            }
+
+            return sb.ToString();
+        }
+
+        public string GetCPUInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+
+            int i = 0;
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                sb.AppendLine(string.Format("CPU {0} : {1} ({2})", i.ToString(), obj["Name"].ToString(), obj["Description"].ToString()));
+                i++;
+            }
+
+            return sb.ToString();
+        }
+
+
+        public string GetGPUInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DisplayConfiguration");
+            int i = 0;
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                sb.AppendLine(string.Format("GPU {0} : {1}", i.ToString(), obj["Description"].ToString()));
+                i++;
+            }
+
+            return sb.ToString();
+        }
+
+        public string GetDiskInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            sb.AppendLine(string.Format("Drive Information ( System Drive : {0} )", System.Environment.GetEnvironmentVariable("SystemDrive")));
+
+            int i = 0;
+            foreach (DriveInfo drive in drives)
+            {
+                string strTotalSize = Convert.ToInt32(drive.TotalSize / 1024 / 1024 / 1024).ToString();
+                string strFreeSize = Convert.ToInt32(drive.AvailableFreeSpace / 1024 / 1024 / 1024).ToString();
+                int iUsageSize = Convert.ToInt32(strTotalSize) - Convert.ToInt32(strFreeSize);
+                string strUsage = iUsageSize.ToString();
+                string strFormat = drive.DriveFormat;
+                string strType = drive.DriveType.ToString();
+
+                string drvInfo = string.Format("Disk ({0})[ {1}:{2}({3}) ] Total : {4} GB, Usage : {5} GB, Free : {6} GB",
+                                    i.ToString(),
+                                    drive.Name,
+                                    strFormat,
+                                    strType,
+                                    strTotalSize, 
+                                    strUsage, 
+                                    strFreeSize);
+
+                i++;
+
+                sb.AppendLine(drvInfo);
+            }
+
+            return sb.ToString();
+        }
+
+        public string GetNetworkAdapter()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            ObjectQuery oq = new System.Management.ObjectQuery("Select * from Win32_NetworkAdapterConfiguration Where IPEnabled = 'true'");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(oq);
+
+            int i = 0;
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                string macAddress = obj["MACAddress"].ToString();
+                string ipAddress = ((string[])obj["IPAddress"])[0].ToString();
+                string desc = obj["Description"].ToString();
+
+                sb.AppendLine(string.Format("Ethernet ({0} enabled) IPv4 : {1}, MAC : {2} ({3})", i.ToString(), ipAddress, macAddress, desc));
+                i++;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
