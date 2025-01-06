925efeda405b3ed8a1b16e2a84545289adf9fa69
20230206 <berealize@gmail.com>
2025-01-06 09:22:16 +0900

    fsdf

--- NaniyoInfo/Program.cs
+++ NaniyoInfo/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
 
 namespace NaniyoInfo
 {
@@ -13,9 +15,41 @@ namespace NaniyoInfo
         [STAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SystemInfo());
+        }
+
+
+        /// <summary>
+        /// DLL 로드
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private static Assembly ResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            Assembly thisAssembly = Assembly.GetExecutingAssembly();
+            var name = args.Name.Substring(0, args.Name.IndexOf(',')) + ".dll";
+
+            var resources = thisAssembly.GetManifestResourceNames().Where(s => s.EndsWith(name));
+            if (resources.Count() > 0)
+            {
+                string resourceName = resources.First();
+                using (Stream stream = thisAssembly.GetManifestResourceStream(resourceName))
+                {
+                    if (stream != null)
+                    {
+                        byte[] assembly = new byte[stream.Length];
+                        stream.Read(assembly, 0, assembly.Length);
+                        System.Diagnostics.Debug.WriteLine("Dll file load : " + resourceName);
+                        return Assembly.Load(assembly);
+                    }
+                }
+            }
+            return null;
         }
     }
 }
